28f0e12ab8e7f9113852c3d06659ca4e8110c591
Keijiro_Ida <idatt1122@gmail.com>
2024-08-18 20:38:03 +0900

    vite無効化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @if (!env('VITE_DISABLE', false))
+            {{ vite(['resources/js/app.js', 'resources/css/app.css']) }}
+        @endif
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
